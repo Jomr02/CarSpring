4a49ef9fccba7071e9a0a91e7a282d2fff9ba4aa
Jomr02 <78909730+Jomr02@users.noreply.github.com>
2021-02-14 20:29:01 +0100

    Primera aproximación
    Htmls creados, controllers creados, clases creadas para ir enfocando la BBDD

---
+++ DAD_CarSpring/src/main/java/es/urjc/etsii/dad/CarSpring/AnunciosRepository.java
@@ -0,0 +1,7 @@
+package es.urjc.etsii.dad.CarSpring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnunciosRepository extends JpaRepository<AnuncioVenta, Long> {
+
+}
---
+++ DAD_CarSpring/src/main/java/es/urjc/etsii/dad/CarSpring/UsuariosRepository.java
@@ -0,0 +1,8 @@
+package es.urjc.etsii.dad.CarSpring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuariosRepository extends JpaRepository<Usuario, Long> {
+
+
+}
