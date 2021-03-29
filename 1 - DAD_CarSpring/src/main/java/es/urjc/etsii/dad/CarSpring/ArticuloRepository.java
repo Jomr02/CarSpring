02a3a9a4059d6a0d7254cc7043571103f637167c
Jomr02 <78909730+Jomr02@users.noreply.github.com>
2021-03-29 11:53:49 +0200

    Delete 1 - DAD_CarSpring directory

--- 1 - DAD_CarSpring/src/main/java/es/urjc/etsii/dad/CarSpring/ArticuloRepository.java
+++
@@ -1,7 +0,0 @@
-package es.urjc.etsii.dad.CarSpring;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticuloRepository extends JpaRepository<Articulo, Long> {
-
-}
--- 1 - DAD_CarSpring/src/main/java/es/urjc/etsii/dad/CarSpring/MensajesRepository.java
+++
@@ -1,7 +0,0 @@
-package es.urjc.etsii.dad.CarSpring;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MensajesRepository extends JpaRepository<Mensaje, Long> {
-
-}
