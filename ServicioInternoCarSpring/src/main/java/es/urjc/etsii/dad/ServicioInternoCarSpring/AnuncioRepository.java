ede677cc48104429c4856a9459f9bb5fa7f85dfb
Jomr02 <78909730+Jomr02@users.noreply.github.com>
2021-04-06 13:39:51 +0200

    Primera Aproximacion Servicio Interno

---
+++ ServicioInternoCarSpring/src/main/java/es/urjc/etsii/dad/ServicioInternoCarSpring/AnuncioRepository.java
@@ -0,0 +1,11 @@
+package es.urjc.etsii.dad.ServicioInternoCarSpring;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnuncioRepository extends JpaRepository<Anuncio, Long> {
+	List<Anuncio> findByAnunciante_Id(Long id);
+	List<Anuncio> findByAnunciante_Nick(String nick);
+	long deleteByAnunciante_Id(Long id);
+} 
